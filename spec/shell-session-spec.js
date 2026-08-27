8bb4dbdd1eb27671abb7679124e866a9b23e4639
Sebastian Balcerowiak <asiloisad.93@gmail.com>
2026-08-27 22:42:25 +0200

    Move scoped settings to editor namespace

--- spec/shell-session-spec.js
+++ spec/shell-session-spec.js
@@ -2,7 +2,7 @@ describe("Shell session grammar", () => {
   let grammar = null;
 
   beforeEach(async () => {
-    lumine.config.set("language.useTreeSitterParsers", false);
+    lumine.config.set("editor.useTreeSitterParsers", false);
 
     await lumine.packages.activatePackage("language-shellscript");
 
--- spec/shell-unix-bash-spec.js
+++ spec/shell-unix-bash-spec.js
@@ -14,7 +14,7 @@ describe("Shell script grammar", function () {
   let grammar = null;
 
   beforeEach(async () => {
-    lumine.config.set("language.useTreeSitterParsers", false);
+    lumine.config.set("editor.useTreeSitterParsers", false);
 
     await lumine.packages.activatePackage("language-shellscript");
 
